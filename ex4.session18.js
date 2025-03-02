6885d94c0e20fefc9843a3c317f4896f34318e62
duc123694 <tienducle86@gmail.com>
2025-03-02 20:02:35 +0700

    Add files via upload

---
+++ ex4.session18.js
@@ -0,0 +1,41 @@
+let number = prompt("Mời nhập một số từ 0 đến 9");
+
+if (number !== null) {
+    switch (number) {
+        case "0":
+            document.write("Số Không");
+            break;
+        case "1":
+            document.write("Số Một");
+            break;
+        case "2":
+            document.write("Số Hai");
+            break;
+        case "3":
+            document.write("Số Ba");
+            break;
+        case "4":
+            document.write("Số Bốn");
+            break;
+        case "5":
+            document.write("Số Năm");
+            break;
+        case "6":
+            document.write("Số Sáu");
+            break;
+        case "7":
+            document.write("Số Bảy");
+            break;
+        case "8":
+            document.write("Số Tám");
+            break;
+        case "9":
+            document.write("Số Chín");
+            break;
+        default:
+            document.write("Vui lòng nhập một số hợp lệ từ 0 đến 9");
+    }
+} else {
+    document.write("Vui lòng nhập một số hợp lệ từ 0 đến 9");
+}
+
